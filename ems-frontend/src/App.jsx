1d01894f6982068edf261468cc1ea1e8e4ccb71e
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-03-14 15:41:37 -0400

    listEmployees rendered"

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import HelloWorld from './HelloWorld'
+
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <HelloWorld/>
+    
+     <HeaderComponent />
+     <ListEmployeeComponent />
+     <FooterComponent />
     </>
   )
 }
---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <nav className='navbar'>
+      <a class="navbar-brand" href="#">Employee Management Service</a>
+
+      </nav>
+    </div>
+  )
+}
+
+export default HeaderComponent
